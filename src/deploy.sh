0cb89ed7c5dc17073f65d9316a643d4515b4f1a6
Nodir <nodirhikmatov6gmail.com>
2025-06-23 08:15:23 +0900

    feat:created deploy.sh

---
+++ src/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# production
+git reset --hard
+git checkout develop 
+git pull origin develop
+
+npm i yarn -g
+yarn global add serve
+yarn 
+yarn run build
+pm2 start "yarn run start:prod" --name=Pawfect-React
+
